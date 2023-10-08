55a37f14404f4848384e730963a8616beb7ce058
Myke <mioman72@gmail.com>
2023-10-08 16:35:06 +0300

    second function first commit

---
+++ js/main.js
@@ -0,0 +1,38 @@
+const students = [
+  { name: 'Ivan', age: 20 },
+  { name: 'Petro', age: 30 },
+  { name: 'Yulia', age: 25 },
+  { name: 'Tanya', age: 27 },
+  { name: 'Tanya', age: 30 },
+  { name: 'Vasya', age: 20 },
+];
+
+let students2 = students;
+console.log('students2', students2);
+
+const res2 = delItem(
+  students2,
+  function(student2) {
+      console.log(`Функція 2 - массив без одного елемента`);
+    return student2.age === 30;
+  }
+);
+
+console.log(res2);
+
+function delItem(arr, callback) {
+  let arrRes2 = arr;
+  console.log(`arrRes2`,arrRes2);
+
+
+  for (let i = 0; i < arr.length; i++) {
+    let student2 = arr[i];
+
+    if (callback(student2, i)) {
+
+      arrRes2.splice(i,1);
+      break
+    }
+  }
+  return arrRes2;
+}
